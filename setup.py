b6beeb05aae36dede5abad7581ee7da54d12e0e2
Gabriella Reggiano <reggiano@uw.edu>
2022-10-10 13:47:19 -0700

    added requirements.txt file for installation, haven't tested whether this works with a medic script, but the setup.py runs

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@
 basic setup to start
 """
 from setuptools import setup, find_packages
+import os
 
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+long_description = open(os.path.join(lib_folder,'README.md')).read()
+
+requirements_path = os.path.join(lib_folder, 'requirements.txt')
+print(requirements_path)
+install_requires = list()
+if os.path.isfile(requirements_path):
+	with open(requirements_path) as f:
+		install_requires = f.read().splitlines()
+print(install_requires)
 setup(name="medic",
 	version='0.1dev',
 	description="Model Error Detection in Cryo-EM",
-	long_description=open('README.md').read(),
+	long_description=long_description,
 	author="Gabriella Reggiano",
 	packages=find_packages(exclude=["medic_model"]),
-	include_package_data=True
+	include_package_data=True,
+	install_requires=install_requires
 )
